59276216f3527175ac80cb9ff6588885270d60c8
Bartosz Rozwarski <bartus000@gmail.com>
2024-09-13 10:12:40 +0200

    update signer

--- Example/Shared/Signer/Signer.swift
+++ Example/Shared/Signer/Signer.swift
@@ -16,28 +16,51 @@ struct SendCallsParams: Codable {
     }
 }
 
+enum SmartAccountType {
+    case simple
+    case safe
+}
+
 final class Signer {
     enum Errors: Error {
         case notImplemented
+        case unknownSmartAccountType
     }
-    
+
     private init() {}
 
     static func sign(request: Request, importAccount: ImportAccount) async throws -> AnyCodable {
-        if try await didRequestSmartAccount(request) {
-            return try await signWithSmartAccount(request: request)
+        if let accountType = try await getRequestedSmartAccountType(request) {
+            return try await signWithSmartAccount(request: request, accountType: accountType)
         } else {
             return try signWithEOA(request: request, importAccount: importAccount)
         }
     }
 
-    private static func didRequestSmartAccount(_ request: Request) async throws -> Bool {
+    private static func getRequestedSmartAccountType(_ request: Request) async throws -> SmartAccountType? {
+        let account = try await getRequestedAccount(request)
+        if account == nil {
+            return nil
+        }
+
+        let simpleSmartAccountAddress = try await SmartAccount.instance.getClient().getAddress()
+        let safeSmartAccountAddress = try await SmartAccountSafe.instance.getClient().getAddress()
+
+        if account?.lowercased() == simpleSmartAccountAddress.lowercased() {
+            return .simple
+        } else if account?.lowercased() == safeSmartAccountAddress.lowercased() {
+            return .safe
+        }
+
+        return nil
+    }
+
+    private static func getRequestedAccount(_ request: Request) async throws -> String? {
         // Attempt to decode params for transaction requests encapsulated in an array of dictionaries
         if let paramsArray = try? request.params.get([AnyCodable].self),
            let firstParam = paramsArray.first?.value as? [String: Any],
            let account = firstParam["from"] as? String {
-            let smartAccountAddress = try await SmartAccount.instance.getClient().getAddress()
-            return account.lowercased() == smartAccountAddress.lowercased()
+            return account
         }
 
         // Attempt to decode params for signing message requests
@@ -46,21 +69,19 @@ final class Signer {
                 // Typically, the account address is the second parameter for personal_sign and eth_signTypedData
                 if paramsArray.count > 1,
                    let account = paramsArray[1].value as? String {
-                    let smartAccountAddress = try await SmartAccount.instance.getClient().getAddress()
-                    return account.lowercased() == smartAccountAddress.lowercased()
+                    return account
                 }
             }
             // Handle the `wallet_sendCalls` method
             if request.method == "wallet_sendCalls" {
                 if let sendCallsParams = paramsArray.first?.value as? [String: Any],
                    let account = sendCallsParams["from"] as? String {
-                    let smartAccountAddress = try await SmartAccount.instance.getClient().getAddress()
-                    return account.lowercased() == smartAccountAddress.lowercased()
+                    return account
                 }
             }
         }
 
-        return false
+        return nil
     }
 
     private static func signWithEOA(request: Request, importAccount: ImportAccount) throws -> AnyCodable {
@@ -80,34 +101,38 @@ final class Signer {
             return SOLSigner.signTransaction(request.params)
 
         default:
-            throw Signer.Errors.notImplemented
+            throw Errors.notImplemented
         }
     }
 
+    private static func signWithSmartAccount(request: Request, accountType: SmartAccountType) async throws -> AnyCodable {
+        let client: AccountClientProtocol
+        switch accountType {
+        case .simple:
+            client = await SmartAccount.instance.getClient()
+        case .safe:
+            client = await SmartAccountSafe.instance.getClient()
+        }
 
-    private static func signWithSmartAccount(request: Request) async throws -> AnyCodable {
         switch request.method {
         case "personal_sign":
             let params = try request.params.get([String].self)
             let message = params[0]
-            let client = await SmartAccount.instance.getClient()
             let signedMessage = try client.signMessage(message)
             return AnyCodable(signedMessage)
 
         case "eth_signTypedData":
             let params = try request.params.get([String].self)
             let message = params[0]
-            let client = await SmartAccount.instance.getClient()
             let signedMessage = try client.signMessage(message)
             return AnyCodable(signedMessage)
 
         case "eth_sendTransaction":
             let params = try request.params.get([YttriumWrapper.Transaction].self)
             let transaction = params[0]
-            let result = try await SmartAccount.instance.getClient().sendTransaction(transaction)
+            let result = try await client.sendTransaction(transaction)
             return AnyCodable(result)
 
-            // sendCalls should handle the whole batch
         case "wallet_sendCalls":
             let params = try request.params.get([SendCallsParams].self)
             guard let firstCall = params.first?.calls.first else {
@@ -120,11 +145,11 @@ final class Signer {
                 data: firstCall.data!
             )
 
-            let result = try await SmartAccount.instance.getClient().sendTransaction(transaction)
+            let result = try await client.sendTransaction(transaction)
             return AnyCodable(result)
 
         default:
-            throw Signer.Errors.notImplemented
+            throw Errors.notImplemented
         }
     }
 }
@@ -133,7 +158,7 @@ extension Signer.Errors: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .notImplemented:   return "Requested method is not implemented"
+        case .unknownSmartAccountType: return "Unknown smart account type"
         }
     }
 }
-
