27ac3a625f455ad262b496e598c15ab592bb75d5
llbartekll <bartus000@gmail.com>
2024-10-08 14:36:28 +0200

    savepoint

--- Sources/WalletConnectRelay/BundleFinder.swift
+++ Sources/WalletConnectRelay/BundleFinder.swift
@@ -1,9 +1,41 @@
-
 import Foundation
 
-extension Bundle {
-    // This will provide access to the resources for the WalletConnectRelay module
-    static var resourceBundle: Bundle {
+private class BundleFinder {}
+
+extension Foundation.Bundle {
+    /// Returns the resource bundle associated with the current Swift module.
+    static var resourceBundle: Bundle = {
+        let bundleName = "reown_WalletConnectRelay"
+        let candidates = [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: BundleFinder.self).resourceURL,
+            // For command-line tools.
+            Bundle.main.bundleURL,
+            // Bundle should be present here when running tests.
+            Bundle(for: BundleFinder.self).resourceURL?
+                .deletingLastPathComponent()
+                .deletingLastPathComponent()
+                .deletingLastPathComponent(),
+            // Other possibilities
+            Bundle(for: BundleFinder.self).resourceURL?
+                .deletingLastPathComponent()
+                .deletingLastPathComponent(),
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+
+        // If we can't find the bundle, fall back to the module bundle if available
+        #if SWIFT_PACKAGE
         return Bundle.module
-    }
+        #else
+        return Bundle(for: BundleFinder.self)
+        #endif
+    }()
 }
