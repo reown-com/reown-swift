d931092913732799df328605df683460096fc351
llbartekll <bartus000@gmail.com>
2024-11-25 09:05:26 +0100

    get nonce

--- Example/WalletApp/PresentationLayer/Wallet/CATransactionModal/CATransactionPresenter.swift
+++ Example/WalletApp/PresentationLayer/Wallet/CATransactionModal/CATransactionPresenter.swift
@@ -4,6 +4,11 @@ import Web3
 import ReownWalletKit
 
 final class CATransactionPresenter: ObservableObject {
+    enum Errors: Error {
+        case invalidURL
+        case invalidResponse
+        case invalidData
+    }
     // Published properties to be used in the view
     @Published var payingAmount: Double = 10.00
     @Published var balanceAmount: Double = 5.00
@@ -55,11 +60,11 @@ final class CATransactionPresenter: ObservableObject {
                 print(signedTransactions[0])
                 try await chainAbstractionService.broadcastTransactions(transactions: signedTransactions)
                 let orchestrationId = routeResponseAvailable.orchestrationId
-//                let statusResponseCompleted = try await WalletKit.instance.waitForSuccess(orchestrationId: orchestrationId, checkIn: routeResponseAvailable.metadata.checkIn)
+                //                let statusResponseCompleted = try await WalletKit.instance.waitForSuccess(orchestrationId: orchestrationId, checkIn: routeResponseAvailable.metadata.checkIn)
 
 
                 var status: StatusResponse = try await WalletKit.instance.status(orchestrationId: orchestrationId)
-                while true {
+                loop: while true {
                     switch status {
                     case .pending(let pending):
                         let delay = try UInt64(pending.checkIn) * 1_000_000
@@ -70,12 +75,13 @@ final class CATransactionPresenter: ObservableObject {
                         // Handle the completed status
                         print("Transaction completed: \(completed)")
                         AlertPresenter.present(message: "routing transactions completed", type: .success)
+                        break loop // Exit the labeled loop but continue with the function
                     case .error(let error):
                         // Handle the error
                         print("Transaction failed: \(error)")
                         AlertPresenter.present(message: "routing failed with error: \(error)", type: .error)
                         ActivityIndicatorManager.shared.stop()
-                        return
+                        return // Exit the function due to an error
                     }
                 }
 
@@ -96,7 +102,7 @@ final class CATransactionPresenter: ObservableObject {
             let to: String
         }
 
-
+        print("will send initial transaction")
         let tx = try! sessionRequest.params.get([Tx].self)[0]
 
 
@@ -105,15 +111,17 @@ final class CATransactionPresenter: ObservableObject {
 
         let maxPriorityFeePerGas = EthereumQuantity(quantity: BigUInt(estimates.maxPriorityFeePerGas, radix: 10)!)
         let maxFeePerGas = EthereumQuantity(quantity: BigUInt(estimates.maxFeePerGas, radix: 10)!)
+        let from = try EthereumAddress(hex: tx.from, eip55: false)
 
+        let nonce = try await getNonce(for: from, chainId: sessionRequest.chainId.absoluteString)
 
         let ethTransaction = EthereumTransaction(
-            nonce: 0,
+            nonce: nonce,
             gasPrice: nil,
             maxFeePerGas: maxFeePerGas,
             maxPriorityFeePerGas: maxPriorityFeePerGas,
             gasLimit: EthereumQuantity(quantity: 1023618),
-            from: try EthereumAddress(hex: tx.from, eip55: false),
+            from: from,
             to: try EthereumAddress(hex: tx.to, eip55: false),
             value: EthereumQuantity(quantity: 0.gwei),
             data: EthereumData(Array(hex: tx.data)),
@@ -131,6 +139,56 @@ final class CATransactionPresenter: ObservableObject {
 
     }
 
+    func getNonce(for address: EthereumAddress, chainId: String) async throws -> EthereumQuantity {
+        // Create a Web3 provider
+
+        print("Getting nonce for initial transaction...")
+        let projectId = Networking.projectId
+        let rpcUrl = "rpc.walletconnect.com/v1?chainId=\(chainId)&projectId=\(projectId)"
+
+        let params = [address.hex(eip55: true), "latest"]
+
+        let rpcRequest = RPCRequest(method: "eth_getTransactionCount", params: params)
+
+        guard let url = URL(string: "https://" + rpcUrl) else {
+            throw Errors.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        // Convert RPC request to JSON data
+        let jsonData = try JSONEncoder().encode(rpcRequest)
+        request.httpBody = jsonData
+
+        do {
+            // Perform the URL session request
+            let (data, response) = try await URLSession.shared.data(for: request)
+
+            // Validate the response
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                throw Errors.invalidResponse
+            }
+
+            let rpcResponse = try JSONDecoder().decode(RPCResponse.self, from: data)
+
+            let responseJSON = try JSONSerialization.jsonObject(with: data)
+            print(responseJSON)
+
+            let stringResult = try rpcResponse.result!.get(String.self)
+            guard let nonceValue = BigUInt(stringResult.stripHexPrefix(), radix: 16) else {
+                throw Errors.invalidData
+            }
+
+            return EthereumQuantity(quantity: nonceValue)
+        } catch {
+            print("Error fetching nonce: \(error)")
+            throw error
+        }
+    }
+
     @MainActor
     func rejectTransactions() async throws {
         do {
@@ -145,8 +203,8 @@ final class CATransactionPresenter: ObservableObject {
         } catch {
             ActivityIndicatorManager.shared.stop()
             AlertPresenter.present(message: error.localizedDescription, type: .error)
-//            errorMessage = error.localizedDescription
-//            showError.toggle()
+            //            errorMessage = error.localizedDescription
+            //            showError.toggle()
         }
     }
 
@@ -166,7 +224,7 @@ final class CATransactionPresenter: ObservableObject {
         let usdcValue = Double(indecValue) / 1_000_000
         return String(format: "%.2f", usdcValue)
     }
-    
+
     func hexToDecimal(_ hex: String) -> Int? {
         let cleanHex = hex.hasPrefix("0x") ? String(hex.dropFirst(2)) : hex
 
