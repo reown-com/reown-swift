e2fb2d3ecba8730302a7711a0b5d88ba6118039d
llbartekll <bartus000@gmail.com>
2024-12-04 09:03:40 +0100

    add bundle id query param

--- Sources/WalletConnectRelay/RelayURLFactory.swift
+++ Sources/WalletConnectRelay/RelayURLFactory.swift
@@ -12,13 +12,16 @@ class RelayUrlFactory {
         self.projectId = projectId
     }
 
-    func create() -> URL {
+    func create(bundleId: String?) -> URL {
         var components = URLComponents()
         components.scheme = "wss"
         components.host = relayHost
         components.queryItems = [
             URLQueryItem(name: "projectId", value: projectId)
         ]
+        if let bundleId = Bundle.main.bundleIdentifier {
+            components.queryItems?.append(URLQueryItem(name: "bundleId", value: bundleId))
+        }
         return components.url!
     }
 }
