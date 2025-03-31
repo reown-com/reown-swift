194996e4e35faaef58920c8ff861809d1a4292a9
Bartosz Rozwarski <>
2025-03-31 14:58:50 +0200

    savepoint

--- Tests/WalletConnectPairingTests/AppMetadataTests.swift
+++ Tests/WalletConnectPairingTests/AppMetadataTests.swift
@@ -1,6 +1,36 @@
 import XCTest
 @testable import WalletConnectPairing
 
+final class AppMetadataTests: XCTestCase {
+    
+    func testDecodingWithMissingUrlProperty() throws {
+        let json = """
+        {
+            "name": "Test App",
+            "description": "Test Description",
+            "icons": ["https://test.com/icon.png"]
+        }
+        """.data(using: .utf8)!
+        
+        let metadata = try JSONDecoder().decode(AppMetadata.self, from: json)
+        XCTAssertEqual(metadata.url, "")
+    }
+    
+    func testDecodingWithPresentUrlProperty() throws {
+        let json = """
+        {
+            "name": "Test App",
+            "description": "Test Description",
+            "url": "https://test.com",
+            "icons": ["https://test.com/icon.png"]
+        }
+        """.data(using: .utf8)!
+        
+        let metadata = try JSONDecoder().decode(AppMetadata.self, from: json)
+        XCTAssertEqual(metadata.url, "https://test.com")
+    }
+}
+
 final class RedirectTests: XCTestCase {
 
     func testInitThrowsErrorWhenLinkModeIsTrueAndUniversalIsNil() {
