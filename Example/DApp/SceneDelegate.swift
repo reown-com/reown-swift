82bab2111a312db6953fdfd2517c54efb87a5f4e
Bartosz Rozwarski <bartus000@gmail.com>
2025-06-04 13:07:55 +0200

    savepoint

--- Example/DApp/SceneDelegate.swift
+++ Example/DApp/SceneDelegate.swift
@@ -107,23 +107,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             recommendedWalletIds: ["38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96"],
             customWallets: [
                 .init(
-                    id: "web-test",
-                    name: "web test",
-                    homepage: "https://walletconnect.com/",
-                    imageUrl: "https://avatars.githubusercontent.com/u/37784886?s=200&v=4",
-                    order: 1,
-                    mobileLink: nil,
-                    linkMode: nil,
-                    webappLink: "https://feat-ww-encode-personal-sign.appkit-web-wallet.pages.dev"
-                ),
-                .init(
                     id: "swift-sample",
                     name: "Swift Sample Wallet",
                     homepage: "https://walletconnect.com/",
                     imageUrl: "https://avatars.githubusercontent.com/u/37784886?s=200&v=4",
                     order: 1,
                     mobileLink: "walletapp://",
-                    linkMode: nil
+                    linkMode: "https://lab.web3modal.com/wallet"
                 ),
                 .init(
                     id: "rn-sample-internal",
