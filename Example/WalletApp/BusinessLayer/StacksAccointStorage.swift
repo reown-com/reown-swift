dbcf715bc7f6de6fc263d70999542d56866a4be1
Bartosz Rozwarski <bartus000@gmail.com>
2025-06-17 09:17:51 +0200

    add StacksAccountStorage

---
+++ Example/WalletApp/BusinessLayer/StacksAccointStorage.swift
@@ -0,0 +1,36 @@
+import Foundation
+import ReownWalletKit
+
+class StacksAccountStorage {
+    enum Errors: Error {
+        case invalidWallet
+    }
+    
+    static var storageKey = "stacks_wallet"
+    private let chainId: Blockchain = Blockchain("stacks:1")!
+    
+    func getAddress() throws -> String? {
+        guard let wallet = getWallet() else { return nil }
+        return try stacksGetAddress(wallet: wallet, version: "1")
+    }
+    
+    func getCaip10Account() throws -> ReownWalletKit.Account? {
+        guard let address = try getAddress() else { return nil }
+        return Account(blockchain: chainId, address: address)!
+    }
+    
+    func saveWallet(wallet: String) {
+        UserDefaults.standard.set(wallet, forKey: Self.storageKey)
+    }
+    
+    func getWallet() -> String? {
+        return UserDefaults.standard.string(forKey: Self.storageKey)
+    }
+    
+    @discardableResult
+    func generateAndSaveWallet() -> String? {
+        let mnemonic = stacksGenerateWallet()
+        saveWallet(wallet: mnemonic)
+        return mnemonic
+    }
+}
